26badf1b546a8d9c01a0b4f5727ff97bd5f4d20b
alejandrotorrv <alejandrotorrv@outlook.com>
2024-09-22 03:39:52 -0600

    video netflix eureka client funcionando

---
+++ springboot-service-celularess/src/test/java/com/microservice/celulares/SpringbootServiceCelularessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.celulares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCelularessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
